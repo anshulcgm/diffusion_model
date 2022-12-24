9381ab8209fe71a4d9d0b15d1c6fe226fb895565
Anshul Ahluwalia <anshul.cgm@gmail.com>
2022-12-24 14:07:13 -0800

    Reformatted files

--- diffusion/models/gaussian_diffusion.py
+++ diffusion/models/gaussian_diffusion.py
@@ -5,7 +5,9 @@ import torch.nn as nn
 
 
 class GaussianDiffusion(nn.Module):
-    def __init__(self, n_timesteps: int, variance_lower_bound: float = 0.001, variance_upper_bound: float = 0.002) -> None:
+    def __init__(
+        self, n_timesteps: int, variance_lower_bound: float = 0.001, variance_upper_bound: float = 0.002
+    ) -> None:
         """Define variance schedule along with important constants for calculations
 
         Args:
@@ -20,22 +22,22 @@ class GaussianDiffusion(nn.Module):
         self.variance_end = 1000 / self.n_timesteps * self.variance_upper_bound
         self.betas = torch.linspace(self.variance_start, self.variance_end, self.n_timesteps)
 
-        self.alphas = 1. - self.betas
-        self.cumulative_alphas = torch.cumprod(self.alphas, axis = 0)
-    
+        self.alphas = 1.0 - self.betas
+        self.cumulative_alphas = torch.cumprod(self.alphas, axis=0)
+
     def add_noise(self, batch: torch.Tensor, timesteps: torch.Tensor) -> torch.Tensor:
         """Adds gaussian noise to each image in the batch depending on the timestep
 
         Args:
             batch: Tensor of shape (b, c, h, w) representing images
             timesteps: Tensor of shape (b,) representing which timestep each batch is in
-        
+
         Returns:
             noised_batch: Tensor of shape (b, c, h, w) representing noise added to batch
         """
         b, _, _, _ = batch.shape
         curr_alphas = self.cumulative_alphas[timesteps].unsqueeze(1).unsqueeze(2).unsqueeze(3)
-        curr_variances = 1. - curr_alphas
-        random_noise = torch.randn(size = [b, 1, 1, 1])
+        curr_variances = 1.0 - curr_alphas
+        random_noise = torch.randn(size=[b, 1, 1, 1])
         noised_batch = torch.sqrt(curr_alphas) * batch + torch.sqrt(curr_variances) * random_noise
         return noised_batch
--- tests/models/test_gaussian_diffusion.py
+++ tests/models/test_gaussian_diffusion.py
@@ -8,15 +8,17 @@ from diffusion.models.gaussian_diffusion import GaussianDiffusion
 random.seed(42)
 torch.manual_seed(42)
 
+
 def test_init():
     num_timesteps = random.randint(10, 30)
     model = GaussianDiffusion(num_timesteps)
+
 
 def test_add_noise():
     num_timesteps = random.randint(10, 30)
     batch_size = random.randint(4, 10)
     model = GaussianDiffusion(num_timesteps)
-    random_img_tensor = torch.rand(size = [batch_size, 3, 224, 224])
-    curr_timesteps = torch.randint(low = 0, high = num_timesteps, size = [batch_size])
+    random_img_tensor = torch.rand(size=[batch_size, 3, 224, 224])
+    curr_timesteps = torch.randint(low=0, high=num_timesteps, size=[batch_size])
     noised_batch = model.add_noise(random_img_tensor, curr_timesteps)
     assert noised_batch.shape == random_img_tensor.shape
