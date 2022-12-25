dec0033b84378d562a3463718a05666a56209c73
Anshul Ahluwalia <anshul.cgm@gmail.com>
2022-12-24 16:25:54 -0800

    added method to calculate initial images given noise and current images

--- diffusion/models/gaussian_diffusion.py
+++ diffusion/models/gaussian_diffusion.py
@@ -26,13 +26,15 @@ class GaussianDiffusion(nn.Module):
         self.alphas = 1.0 - self.betas
         self.cumulative_alphas = torch.cumprod(self.alphas, axis=0)
 
-    def add_noise(self, batch: torch.Tensor, timesteps: torch.Tensor, random_noise: Optional[torch.Tensor] = None) -> torch.Tensor:
+    def add_noise(
+        self, batch: torch.Tensor, timesteps: torch.Tensor, random_noise: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
         """Adds gaussian noise to each image in the batch depending on the timestep
 
         Args:
             batch: Tensor of shape (b, c, h, w) representing images
             timesteps: Tensor of shape (b,) representing which timestep each batch is in
-            noise: Tensor of shape (b, 1, 1, 1) representing random noise for each batch
+            noise: Tensor of shape (b,) representing random noise for each batch
 
         Returns:
             noised_batch: Tensor of shape (b, c, h, w) representing noise added to batch
@@ -41,6 +43,28 @@ class GaussianDiffusion(nn.Module):
         curr_alphas = self.cumulative_alphas[timesteps].unsqueeze(1).unsqueeze(2).unsqueeze(3)
         curr_variances = 1.0 - curr_alphas
         if random_noise is None:
-            random_noise = torch.randn(size=[b, 1, 1, 1])
-        noised_batch = torch.sqrt(curr_alphas) * batch + torch.sqrt(curr_variances) * random_noise
+            random_noise = torch.randn(size=[b])
+        noised_batch = torch.sqrt(curr_alphas) * batch + torch.sqrt(curr_variances) * random_noise.unsqueeze(
+            1
+        ).unsqueeze(2).unsqueeze(3)
         return noised_batch
+
+    def calculate_starting_image(
+        self, curr_images: torch.Tensor, curr_timesteps: torch.Tensor, noise: torch.Tensor
+    ) -> torch.Tensor:
+        """Calculates x_0 from x_t
+
+        Args:
+            curr_images: Tensor of shape (b, c, h, w) representing noised images
+            curr_timesteps: Tensor of shape (b,) representing which timestep each element in the batch is at
+            noise: Tensor of shape (b,) indicating noise predictions by decoder network for each batch
+
+        Returns:
+            starting_images: Images before any gaussian noise was added
+        """
+        curr_alphas = self.cumulative_alphas[curr_timesteps].unsqueeze(1).unsqueeze(2).unsqueeze(3)
+        curr_variances = 1.0 - curr_alphas
+        starting_images = 1.0 / torch.sqrt(curr_alphas) * curr_images - torch.sqrt(
+            curr_variances / curr_alphas
+        ) * noise.unsqueeze(1).unsqueeze(2).unsqueeze(3)
+        return starting_images
--- tests/models/test_gaussian_diffusion.py
+++ tests/models/test_gaussian_diffusion.py
@@ -19,23 +19,35 @@ def test_add_noise_shape():
     batch_size = random.randint(4, 10)
     model = GaussianDiffusion(num_timesteps)
     random_img_tensor = torch.rand(size=[batch_size, 3, 224, 224])
-    curr_timesteps = torch.randint(low=0, high=num_timesteps, size=[batch_size])
+    curr_timesteps = torch.randint(low=1, high=num_timesteps, size=[batch_size])
     noised_batch = model.add_noise(random_img_tensor, curr_timesteps)
     assert noised_batch.shape == random_img_tensor.shape
 
+
 def test_add_noise_specific_values():
-    img_tensor = torch.randn(size = [4, 3, 2, 2])
-    random_noise = torch.randn(size = [4, 1, 1, 1])
+    img_tensor = torch.randn(size=[4, 3, 2, 2])
+    random_noise = torch.randn(size=[4])
     noised_batch = torch.zeros_like(img_tensor)
-    model = GaussianDiffusion(n_timesteps = 20)
+    model = GaussianDiffusion(n_timesteps=20)
     timesteps = torch.Tensor([4, 3, 10, 9]).to(torch.int64)
     for i in range(img_tensor.shape[0]):
         curr_img = img_tensor[i]
         curr_timestep = timesteps[i]
         curr_noise = random_noise[i].item()
         alpha_value = model.cumulative_alphas[curr_timestep]
-        variance = 1. - alpha_value
+        variance = 1.0 - alpha_value
         noisy_img = torch.sqrt(alpha_value) * curr_img + torch.sqrt(variance) * curr_noise
         noised_batch[i] = noisy_img
     model_output = model.add_noise(img_tensor, timesteps, random_noise)
     assert torch.equal(model_output, noised_batch)
+
+
+def test_calculate_starting_image():
+    num_timesteps = random.randint(10, 30)
+    img_tensor = torch.randn(size=[4, 3, 2, 2])
+    random_noise = torch.randn(size=[4])
+    timesteps = torch.randint(low=1, high=num_timesteps, size=[4])
+    model = GaussianDiffusion(n_timesteps=num_timesteps)
+    noised_batch = model.add_noise(img_tensor, timesteps, random_noise)
+    calculated_start_images = model.calculate_starting_image(noised_batch, timesteps, random_noise)
+    assert torch.all(torch.isclose(img_tensor, calculated_start_images))
