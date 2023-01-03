e5aa6b3a886d638a13013137494f1c2e2e8aa1e6
Anshul Ahluwalia <anshul.cgm@gmail.com>
2023-01-03 13:26:06 -0800

    Wrote training loop to train denoising model

--- diffusion/data_utils.py
+++ diffusion/data_utils.py
@@ -9,31 +9,34 @@ from torch.utils.data import Dataset, DataLoader
 
 TRAIN_DIR = "archive/car_data/car_data/train/"
 
+
 def load_dataset(train_dir: str) -> Dataset:
     """Creating the dataset for the train images
-    
+
     Args:
         train_dir: Directory where all the images are located
-    
+
     Returns:
         dataset: Dataset object with images all sized to resolution 224x224
     """
     image_transform = transforms.Compose([transforms.Resize([224, 224]), transforms.ToTensor()])
-    dataset = datasets.ImageFolder(train_dir, transform = image_transform)
+    dataset = datasets.ImageFolder(train_dir, transform=image_transform)
     return dataset
+
 
 def load_dataloader(dataset: Dataset, batch_size: int) -> DataLoader:
     """Creating dataloader to run through the dataset
-    
+
     Args:
         dataset: Dataset object containing images and labels for cars dataset
         batch_size: How many images to put in one batch
-    
+
     Return:
         dataloader: Dataloader object for inputted dataset
     """
-    dataloader = DataLoader(dataset, batch_size = batch_size)
+    dataloader = DataLoader(dataset, batch_size=batch_size)
     return dataloader
+
 
 def prep_data(train_dir: str, batch_size: int) -> DataLoader:
     """Simple method to prep the cars dataset and return the dataloader
@@ -41,10 +44,10 @@ def prep_data(train_dir: str, batch_size: int) -> DataLoader:
     Args:
         train_dir: Directory where the images are located
         batch_size: How many images in one batch
-    
+
     Returns:
         dataloader: Dataloader with images all sized to 224x224 resolution
     """
     dataset = load_dataset(train_dir)
-    dataloader = load_dataloader(dataset = dataset, batch_size = batch_size)
+    dataloader = load_dataloader(dataset=dataset, batch_size=batch_size)
     return dataloader
---
+++ diffusion/train.py
@@ -0,0 +1,72 @@
+import pdb
+
+import torch
+from torch.nn import MSELoss
+
+from diffusion.models.gaussian_diffusion import GaussianDiffusion
+from diffusion.models.denoising_model import DenoisingModel
+from diffusion.data_utils import prep_data
+
+NUM_TIMESTEPS = 20
+TIME_EMB_DIM = 64
+NUM_EPOCHS = 100
+
+TRAINING_DIR = "archive/car_data/car_data/train"
+BATCH_SIZE = 8
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def calculate_loss(
+    diffusion_model: GaussianDiffusion,
+    denoising_model: DenoisingModel,
+    x_start: torch.Tensor,
+    timesteps: torch.Tensor,
+    criterion: torch.nn.Module,
+) -> torch.Tensor:
+    """Calculates difference between predicted noise by denoising model and inputted noise into gaussian model
+
+    Args:
+        diffusion_model: noising model
+        denoising_model: encoder-decoder CNN that predicts noise given timesteps
+        x_start: Starting unnoised images
+        timesteps: Which part of the noising process each image in the batch is at
+        criterion: Loss module to calculate loss between predicted noise and actual noise
+    Returns:
+        loss: Difference between predicted noise and inputted noise as estimated by criterion
+    """
+    noise = torch.randn_like(x_start)
+    noised_images = diffusion_model.add_noise(batch=x_start, timesteps=timesteps, random_noise=noise)
+    pred_noise = denoising_model(batch=noised_images, timesteps=timesteps)
+    loss = criterion(pred_noise, noise.to(device))
+    return loss
+
+def train() -> None:
+    """Training Loop for denoising model"""
+    diffusion_model = GaussianDiffusion(n_timesteps = NUM_TIMESTEPS)
+    denoising_model = DenoisingModel(n_timesteps = NUM_TIMESTEPS, time_emb_dim = TIME_EMB_DIM)
+    denoising_model = denoising_model.to(device)
+    training_dataloader = prep_data(train_dir = TRAINING_DIR, batch_size = BATCH_SIZE)
+    print(f"There are {len(training_dataloader)} batches in the loader")
+    criterion = MSELoss()
+    optimizer = torch.optim.Adam(denoising_model.parameters(), lr = 0.001)
+    for i in range(NUM_EPOCHS):
+        total_loss = 0.0
+        for j, (images, _) in enumerate(training_dataloader):
+            optimizer.zero_grad()
+            images = image.to(device)
+            timesteps = torch.randint(low = 1, high = NUM_TIMESTEPS, size = [BATCH_SIZE])
+            loss = calculate_loss(diffusion_model, denoising_model, images, timesteps, criterion)
+            loss.backward()
+            optimizer.step()
+            total_loss = total_loss + loss.item()
+            print(f"Epoch {i + 1}, Batch {j + 1}: Loss = {loss.cpu().item():.4f}")
+        
+        avg_loss = total_loss / len(training_dataloader)
+        print(f"For epoch {i + 1}: Average Loss = {avg_loss:.4f}")
+    
+    torch.save(denoising_model, "trained_denoising_model.pth")
+
+if __name__ == "__main__":
+    train()
+
+
