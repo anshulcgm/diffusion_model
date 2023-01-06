cc893aef7c9c5f7a047d4c02796a3e074d9fb14e
Anshul Ahluwalia <anshul.cgm@gmail.com>
2023-01-06 16:32:05 -0500

    Changed sample method to denoise using model trained on data parallel

--- diffusion/sample.py
+++ diffusion/sample.py
@@ -1,4 +1,5 @@
 import pdb
+import os
 
 import torch
 import torchvision.transforms as T
@@ -8,11 +9,13 @@ from PIL import Image
 from diffusion.models.gaussian_diffusion import GaussianDiffusion
 from diffusion.models.denoising_model import DenoisingModel
 
-NUM_TIMESTEPS = 25
+NUM_TIMESTEPS = 30
 TIME_EMB_DIM = 64
 
 BATCH_SIZE = 4
 IMG_SIZE = 224
+
+SAVED_MODEL_PATH = "trained_denoising_model.pth"
 
 def sample_from_random_noise(diffusion_model: GaussianDiffusion, denoising_model: DenoisingModel) -> torch.Tensor:
     """Samples x_0 from x_T
@@ -51,8 +54,10 @@ def main():
     pdb.set_trace()
     diffusion_model = GaussianDiffusion(n_timesteps = NUM_TIMESTEPS)
     denoising_model = DenoisingModel(n_timesteps = NUM_TIMESTEPS, time_emb_dim = TIME_EMB_DIM)
+    if os.path.exists(SAVED_MODEL_PATH):
+        denoising_model.load_state_dict(torch.load(SAVED_MODEL_PATH).module.state_dict())
     sampled_images = sample_from_random_noise(diffusion_model, denoising_model)
-    sampled_images.clamp_(-1., 1.)
+    sampled_images.clamp_(0., 1.)
     unnormalized_images = (sampled_images + 1) * 0.5
     save_images(unnormalized_images)
     
